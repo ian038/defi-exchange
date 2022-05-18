ea8d81edcfcabffe2e5566221c8f3f3c2130d866
ian038 <ianphua3@gmail.com>
2022-05-17 21:07:13 -0400

    Added utils

---
+++ frontend/utils/index.js
@@ -0,0 +1,7 @@
+import TokenAbi from './TrippyDudesToken.json'
+import ExchangeAbi from './Exchange.json'
+
+export const TOKEN_CONTRACT_ABI = TokenAbi.abi
+export const TOKEN_CONTRACT_ADDRESS = '0xEB6907FB6E4F282bB3e348384b94cE1E09513696'
+export const EXCHANGE_CONTRACT_ABI = ExchangeAbi.abi
+export const EXCHANGE_CONTRACT_ADDRESS = '0x4EAA05C052E7BdaB57F85c0fB8D1737d59dd5E05'
